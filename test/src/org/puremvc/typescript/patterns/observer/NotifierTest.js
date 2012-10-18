343d726fc5b38dbc02ba3abdfc634e490c16f911
tekool <github@tekool.net>
2012-10-18 23:24:03 +0200

    Modified tests header licence

--- test/src/org/puremvc/typescript/patterns/observer/NotifierTest.js
+++ test/src/org/puremvc/typescript/patterns/observer/NotifierTest.js
@@ -1,6 +1,6 @@
 /*
- PureMVC Javascript for Objs port by Frederic Saunier Saunier <frederic.saunier@puremvc.org>
- PureMVC - Copyright(c) 2006-2011 Futurescale, Inc., Some rights reserved.
+ PureMVC Javascript for TypeScript port by Frederic Saunier <frederic.saunier@puremvc.org>
+ PureMVC - Copyright(c) 2006-2012 Futurescale, Inc., Some rights reserved.
  Your reuse is governed by the Creative Commons Attribution 3.0 License
 */
 
