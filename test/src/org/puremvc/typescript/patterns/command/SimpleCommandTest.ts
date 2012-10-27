8d1fcf8228b502d5fd1ebb1bdde750d020c75dff
tekool <github@tekool.net>
2012-10-27 18:20:32 +0200

    Modified references list

--- test/src/org/puremvc/typescript/patterns/command/SimpleCommandTest.ts
+++ test/src/org/puremvc/typescript/patterns/command/SimpleCommandTest.ts
@@ -5,6 +5,7 @@
 */
 
 ///<reference path='../../../../../../../test/lib/YUITest.d.ts'/>
+
 ///<reference path='../../../../../../../src/org/puremvc/typescript/interfaces/INotification.ts'/>
 ///<reference path='../../../../../../../src/org/puremvc/typescript/patterns/observer/Notification.ts'/>
 
