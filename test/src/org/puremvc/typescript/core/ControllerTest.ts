4c054a99b7a3bb24df5e7ff442a899fd75fcd534
tekool <github@tekool.net>
2012-10-27 16:39:33 +0200

    Changed file license header copyright timestamp

--- test/src/org/puremvc/typescript/core/ControllerTest.ts
+++ test/src/org/puremvc/typescript/core/ControllerTest.ts
@@ -1,6 +1,6 @@
 /*
  PureMVC for TypeScript port by Frederic Saunier <frederic.saunier@puremvc.org>
- PureMVC - Copyright(c) 2006-2011 Futurescale, Inc., Some rights reserved.
+ PureMVC - Copyright(c) 2006-2012 Futurescale, Inc., Some rights reserved.
  Your reuse is governed by the Creative Commons Attribution 3.0 License
 */
 
